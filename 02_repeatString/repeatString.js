23685650857efb1c198ad9ea4708dba76e08404f
Adina D <adina.dumitrescu1@gmail.com>
2022-06-07 17:59:42 +0300

    repeat string solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+    let newString ="";
+    if(numberOfTimes < 0){
+        return "ERROR";
+    } else{
+        for(var i=0; i<numberOfTimes; i++){
+            newString = newString+string;
+        }
+        return newString;
+    }
+    
 };
 
 // Do not edit below this line
