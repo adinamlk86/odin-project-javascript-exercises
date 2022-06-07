e720d4deefc21fa6187dbdf196621f78aed82321
Adina D <adina.dumitrescu1@gmail.com>
2022-06-07 18:27:56 +0300

    remove from array solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    
+    for(var i=0; i<array.length; i++){
+        for(var j=0; j<args.length; j++){
+            let index =array.indexOf(args[j]);
+            if(index != -1){
+                array.splice(index,1)
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
