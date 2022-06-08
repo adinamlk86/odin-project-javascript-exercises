c092b52bbba75d7dd475d0545e6fc4d29c12faa0
Adina D <adina.dumitrescu1@gmail.com>
2022-06-08 16:07:50 +0300

    palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let noPunctuation = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    noPunctuation = noPunctuation.replace(/\s/g, '')
+    let array = [...noPunctuation];
+    let length = array.length;
 
+    for(let i=0; i<array.length; i++){
+        if (array[i] != array[length-i -1]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
