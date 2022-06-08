26dd355020e9bfeb9ff963490680d932189bb7d1
Adina D <adina.dumitrescu1@gmail.com>
2022-06-08 14:19:27 +0300

    sum all solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
 
+    if(first < 0 || second < 0 || Number.isInteger(first) == false || Number.isInteger(second) == false ){
+        return "ERROR";
+    }
+
+    if(first < second){
+        for(let i=first; i<=second; i++){
+            sum = sum + i;
+        }
+        return sum;
+    } else {
+        for(let j=first; j>=second; j--){
+            sum = sum + j;
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
