830e170a268ab622590c0462206c812c12465355
Adina D <adina.dumitrescu1@gmail.com>
2022-06-08 15:40:17 +0300

    calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
-};
+const add = function(...args) {
+	let add = 0;
+  for(let i = 0; i< args.length; i++){
+    add = add + args[i];
+  }
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+  return add;
 
 };
 
-const power = function() {
+const subtract = function(a, b) {
+  return a-b;
 	
 };
 
-const factorial = function() {
+const sum = function(array) {
+  let sum = 0;
+
+  for(let i = 0; i< array.length; i++){
+    sum = sum + array[i];
+  }
+
+  return sum;
+};
+
+const multiply = function(array) {
+  let total = 1;
+  for(let i=0; i<array.length;i++){
+    total = total * array[i];
+  }
+  return total;
+};
+
+const power = function(number, power) {
+  let total = 1;
+  while(power !=0){
+    total = total * number;
+    power--;
+  }
+  return total;
 	
+};
+
+const factorial = function(number) {
+  let total = 1;
+  for(let i = 1; i<= number; i++){
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
